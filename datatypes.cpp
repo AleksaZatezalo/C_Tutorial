77b13e28d84e7d8cc1e1e79dd71026faee8b492a
Aleksa <zabumaphu@gmail.com>
2021-01-29 16:30:19 -0500

    First Commit

---
+++ datatypes.cpp
@@ -0,0 +1,14 @@
+//datatypes.cpp
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    char grade = 'A';
+    string phrase = "School Sucks";
+    int age = 15;
+    float flt = 20.1;
+    double dub = 20.22;
+    bool flag = true;
+    
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,12 @@
+//variables.cpp
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    string name = "Aleksa";
+    int age = 22;
+    cout << "My name is " << name << endl;
+    cout << "I am " << age << " years old" << endl; 
+    return 0;
+}
