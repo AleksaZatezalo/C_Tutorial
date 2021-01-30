b0f1ef5ae4a11e3af55b28a039d84583437c1ae3
Aleksa <zabumaphu@gmail.com>
2021-01-30 14:54:16 -0500

    Classes Created.

---
+++ classes.cpp
@@ -0,0 +1,14 @@
+//classes.cpp
+#include <iostream>
+using namespace std;
+
+class Book {
+    public:
+        string title;
+        string author;
+        int pages;
+};
+
+int main() {
+    return 0;
+}
