474eb523ff8363a3d5a5fe0433413bbc36ad9e18
Aleksa <zabumaphu@gmail.com>
2021-01-30 15:02:03 -0500

    Constructors created.

--- classes.cpp
+++ classes.cpp
@@ -7,14 +7,16 @@ class Book {
         string title;
         string author;
         int pages;
+        Book(string atitle, string aauthor, int apages){
+            title = atitle;
+            author = aauthor;
+            pages = apages;
+            cout << "Creating Object" << endl;
+        }
 };
 
 int main() {
-    Book book1;
-    book1.title = "Harry Potter";
-    book1.author = "JK Rowling";
-    book1.pages = 500;
-
+    Book book1("Harry Potter", "J.K. Rowling", 500);
     cout << book1.title << '\n' ;
 
     return 0;
