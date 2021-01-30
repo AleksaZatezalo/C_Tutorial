96428811f66369c9500142e4a43ff36ebafb7baa
Aleksa <zabumaphu@gmail.com>
2021-01-30 14:49:00 -0500

    Pointers finished.

--- pointers.cpp
+++ pointers.cpp
@@ -3,11 +3,18 @@
 using namespace std;
 
 int main() {
+    // Var
     int age = 22;
     double gpa = 3.0;
     string name = "Aleksa";
 
-    cout << "Age pointer: " << &age << "\n";
+    // Pointers
+    int *pAge = &age;
+    double *pGpa = &gpa;
+
+    cout << "Age address: " << &age << "\n";
+    cout << "Age pointer: " << pAge << "\n";
+    cout << "Age deReferenced: " << *pAge << "\n";
 
     return 0;
 }
