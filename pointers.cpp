221d299cc20caf7c8ec563e600084cf52ea1656e
Aleksa <zabumaphu@gmail.com>
2021-01-30 14:46:28 -0500

    Starting with pointers.

---
+++ pointers.cpp
@@ -0,0 +1,13 @@
+//pointers.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 22;
+    double gpa = 3.0;
+    string name = "Aleksa";
+
+    cout << "Age pointer: " << &age << "\n";
+
+    return 0;
+}
